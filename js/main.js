040f23c2a46c6bafe849253e64da7446ea065849
mahmoud-saed <m.kotta00@gmail.com>
2022-07-07 21:31:57 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+my_list = document.getElementById('nav-list');
+
+function showMenu() {
+    my_list.style.cssText = `right:0`;
+}
+function hideMenu() {
+    my_list.style.cssText = `right:-200px`;
+}
